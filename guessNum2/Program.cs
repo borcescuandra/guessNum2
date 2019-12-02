8fce216ebf52578b73c18784b011a71f2d775bf6
borcescuandra <borcescuandra@users.noreply.github.com>
2019-12-02 21:53:12 +0200

    Add project files.

---
+++ guessNum2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessNum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int numarGhicit = rand.Next(0, 100);
+            //int numarGhicit = 30;
+            int user = 0;
+
+            while (user != numarGhicit)
+            {
+                Console.Write("Introduceti un numar de la 0 la 100: ");
+                int.TryParse(Console.ReadLine(), out user);
+
+                if (user > numarGhicit)
+                {
+                    Console.WriteLine("{0} este prea mare!", user);
+                }
+                else if (user < numarGhicit)
+                {
+                    Console.WriteLine("{0} este prea mic!", user);
+                }
+                else
+                {
+                    Console.WriteLine("{0} este numarul corect. Felicitari!", user);
+                }
+            }
+            
+        }
+    }
+}
